d72e5376cab04a9c156772c47b25ca05317ee1c6
Seiichi <26223147+suikan4github@users.noreply.github.com>
2025-05-04 07:23:09 +0900

    script complete. testing.

--- fuyujitaku.sh
+++ fuyujitaku.sh
@@ -1,13 +1,44 @@
 #! /bin/sh
 
+#######################################################################
+#
+# This script is used to enable hibernation.
+#
+#######################################################################
+
+# Required swap size is 2 times the RAM size.
+TARGET_SWAP_SIZE=16G
+
+#----------------------------------------------------------------------
+#
+# Resize the swap file.
+#
+
 # Get the swap file name.
 SWAPFILE=$(swapon --show=NAME --noheadings)
 
 
 sudo swapoff $SWAPFILE
-sudo fallocate -l 16G $SWAPFILE
+if [ $? -ne 0 ]; then
+    echo "Failed to turn off swap file."
+    echo "Aborted."
+    exit 1
+fi
+sudo fallocate -l $TARGET_SWAP_SIZE $SWAPFILE
+if [ $? -ne 0 ]; then
+    echo "Failed to resize swap file."
+    echo "Swap recover with original size."
+    sudo swapon $SWAPFILE
+    echo "Aborted."
+    exit 1
+fi
 sudo mkswap $SWAPFILE
 sudo swapon $SWAPFILE
+
+#----------------------------------------------------------------------
+#
+# Inform swap location to the kernel.
+#
 
 # Get the UUID of the root filesystem (where the swap file stays).
 UUID=$(findmnt / -o UUID --noheadings)
@@ -17,3 +48,80 @@ OFFSET=$(sudo filefrag -v /swapfile | awk '/ 0:/{print substr($4, 1, length($4)-
 
 # Construct the resume option for kernel parameters.
 OPTION="resume=UUID=${UUID} resume_offset=${OFFSET}"
+
+# Save the current GRUB configuration to a temporary file.
+SAVED_GRUB=$(mktemp)
+sudo cp /etc/default/grub $SAVED_GRUB
+# Add the resume option to the GRUB_CMDLINE_LINUX_DEFAULT line in /etc/default/grub.
+sudo sed -i "s|^\(GRUB_CMDLINE_LINUX_DEFAULT=.*\)'.*$|\1 ${OPTION}'|" /etc/default/grub
+if [ $? -ne 0 ]; then
+    echo "Failed to update GRUB configuration."
+    echo "Aborted."
+    exit 1
+fi
+
+# Update the GRUB configuration.
+sudo update-grub
+if [ $? -ne 0 ]; then
+    echo "Failed to update GRUB configuration."
+    # Restore the original GRUB configuration.
+    echo "Restoring original GRUB configuration."
+    sudo mv $SAVED_GRUB /etc/default/grub
+    echo "Aborted."
+    exit 1
+else
+    # remove the temporary file.
+    rm $SAVED_GRUB
+fi
+
+#----------------------------------------------------------------------
+#
+# Configure the Hibernate option in the systemd.
+#
+
+sudo mkdir -p /etc/systemd/sleep.conf.d
+sudo cp /etc/systemd/sleep.conf /etc/systemd/sleep.conf.d/hibernate.conf
+
+sudo sed -i 's|^.*HibernateDelaySec=.*$|HibernateDelaySec=900|' /etc/systemd/sleep.conf.d/hibernate.conf
+if [ $? -ne 0 ]; then
+    echo "Failed to update /etc/systemd/sleep.conf.d/hibernate.conf."
+    echo "Aborted."
+    exit 1
+fi
+
+#----------------------------------------------------------------------
+#
+# Configure the Hibernation policy in the systemd.
+#
+
+# Write rule to allow hibernation for all users.
+cat <<EOF | sudo tee /etc/polkit-1/rules.d/50-hibernate.rules
+polkit.addRule(function(action, subject) {
+    if (action.id == "org.freedesktop.login1.hibernate" ||
+        action.id == "org.freedesktop.login1.hibernate-multiple-sessions" ||
+        action.id == "org.freedesktop.upower.hibernate" ||
+        action.id == "org.freedesktop.login1.handle-hibernate-key" ||
+        action.id == "org.freedesktop.login1.hibernate-ignore-inhibit")
+    {
+        return polkit.Result.YES;
+    }
+});
+EOF
+if  [ $? -ne 0 ]; then
+    echo "Failed to update /etc/polkit-1/rules.d/50-hibernate.rules."
+    echo "Aborted."
+    exit 1
+fi
+
+#----------------------------------------------------------------------
+#
+# Configure the Hibernation policy in the systemd.
+#
+echo "************************************************************"
+echo "All done."
+echo "Please reboot your system to apply the changes."
+echo "After reboot, you can use the following command to hibernate your system:"
+echo "  sudo systemctl hibernate"
+echo "or"
+echo "You can hibernate from the GUI."
+
